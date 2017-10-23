69826421e152a219c84bd8942873849196a9967a
Gabriel McColl <gabriel@cloudconfusion.co.uk>
2017-10-23 09:32:27 +0100

    adding user info and time

--- testconfig.ps1
+++ testconfig.ps1
@@ -20,9 +20,19 @@ configuration TestConfig
             ActionExecutable = "C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe"
             ScheduleType = 'Daily'
             DaysInterval = 1
+            StartTime = [datetime]'23 October 2017 01:00:00'
             
         }
-
+        
+        User Investrics
+        {
+            
+                Ensure = "Present"  # To ensure the user account does not exist, set Ensure to "Absent"
+                UserName = "SomeName"
+                Password = $passwordCred # This needs to be a credential object
+                DependsOn = "[Group]GroupExample" # Configures GroupExample first
+            
+        }
     }
 
     Node NotWebServer
